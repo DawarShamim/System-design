12a4f3248ff8989c689ad5aba6531fb1a2c5ec9e
Dawar Shamim <dawarshamim1@gmail.com>
2026-03-18 00:04:39 +0500

    Composite Design Pattern
    few more patterns

---
+++ Design Patterns/Structural/C#/Composite.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+
+interface ISmartDevice
+{
+    void TurnOn();
+    void TurnOff();
+}
+
+class Device : ISmartDevice
+{
+    private string name;
+
+    public Device(string name)
+    { this.name = name; }
+
+    public void TurnOn() { Console.WriteLine($"{name} turned ON"); }
+
+    public void TurnOff() { Console.WriteLine($"{name} turned OFF"); }
+}
+
+class DeviceGroup : ISmartDevice
+{
+    private string name;
+    private List<ISmartDevice> devices = new List<ISmartDevice>();
+
+    public DeviceGroup(string name) { this.name = name; }
+
+    public void AddDevice(ISmartDevice device) { devices.Add(device); }
+
+    public void TurnOn()
+    {
+        Console.WriteLine($"Turning ON group: {name}");
+        foreach (var device in devices)
+        { device.TurnOn(); }
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine($"Turning OFF group: {name}");
+        foreach (var device in devices)
+        { device.TurnOff(); }
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        DeviceGroup livingRoom = new DeviceGroup("livingRoom");
+        Device TV = new Device("TV");
+        Device Cleaner = new Device("Cleaner");
+        Device CoffeeMaker = new Device("CoffeeMaker");
+
+        livingRoom.AddDevice(TV);
+        livingRoom.AddDevice(Cleaner);
+        livingRoom.AddDevice(CoffeeMaker);
+        livingRoom.TurnOff();
+        TV.TurnOn();
+    }
+}
---
+++ Design Patterns/Structural/C#/Decorator.cs
@@ -0,0 +1,62 @@
+using System;
+
+interface ICoffee
+{
+    decimal Cost();
+    string Description();
+}
+
+class SimpleCoffee : ICoffee
+{
+    public decimal Cost() { return 3.0m; }
+    public string Description()
+    { return "Ingredients: Coffee Beans"; }
+}
+
+abstract class CoffeeDecorator : ICoffee
+{
+    protected ICoffee coffee;
+
+    public CoffeeDecorator(ICoffee coffee)
+    { this.coffee = coffee; }
+
+    public abstract decimal Cost();
+    public abstract string Description();
+}
+
+class Latte : CoffeeDecorator
+{
+    public Latte(ICoffee coffee) : base(coffee) { }
+
+    public override decimal Cost()
+    { return coffee.Cost() + 0.75m; }
+
+    public override string Description()
+    { return $"{coffee.Description()}, Milk"; }
+}
+
+class Americano : CoffeeDecorator
+{
+    public Americano(ICoffee coffee) : base(coffee) { }
+
+    public override decimal Cost()
+    { return coffee.Cost() + 0.50m; }
+
+    public override string Description()
+    { return $"{coffee.Description()}, Water"; }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        ICoffee coffee = new SimpleCoffee();
+        Console.WriteLine($"{coffee.Description()} | Cost: {coffee.Cost()}");
+
+        coffee = new Latte(coffee);
+        Console.WriteLine($"{coffee.Description()} | Cost: {coffee.Cost()}");
+
+        coffee = new Americano(coffee);
+        Console.WriteLine($"{coffee.Description()} | Cost: {coffee.Cost()}");
+    }
+}
